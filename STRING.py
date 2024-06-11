9678246d6e489dfe2471816ee46e6553af7b4a4f
SUGAR DADDY 🗿 <140779732+CODE-WITH-MASKMAN@users.noreply.github.com>
2024-06-11 12:09:47 +0545

    Update STRING.py

--- STRING.py
+++ STRING.py
@@ -6,8 +6,8 @@ if bit == '64bit':
 	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m UPDATING TOOL RXS !');time.sleep(1.0)
 	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m OPENING TOOL RXS !');time.sleep(1.0)
 	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;0;255;127m YOUR DEVICE IS 64 BIT');time.sleep(1.0)
-	os.system('chmod +x ./ENC.cpython-311.so')
-	os.system('./ENC.cpython-311.so')
+	os.system('chmod +x ./bit.cpython-311.so')
+	os.system('./bit.cpython-311.so')
 elif bit == '32bit':
 	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;0;255;127m YOUR DEVICE IS 32 BIT');time.sleep(1.0)
 	exit('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m 32 BIT WILL UPLOAD SOON !')
