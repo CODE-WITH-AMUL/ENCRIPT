bb340c6a3ad72d23a8f428b8fb042e81641e842a
SUGAR DADDY 🗿 <140779732+CODE-WITH-MASKMAN@users.noreply.github.com>
2024-06-11 12:25:03 +0545

    Update STRING.py

--- STRING.py
+++ STRING.py
@@ -1,15 +1,17 @@
 import os,platform,time
-try:os.system('git pull')
-except:pass
 bit = platform.architecture()[0]
 if bit == '64bit':
-	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m UPDATING TOOL RXS !');time.sleep(1.0)
-	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m OPENING TOOL RXS !');time.sleep(1.0)
+	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m UPDATING TOOL OF SUGAR DADDY!');time.sleep(1.0)
+	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m OPENING TOOL SUGAR DADDY!');time.sleep(1.0)
 	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;0;255;127m YOUR DEVICE IS 64 BIT');time.sleep(1.0)
-	os.system('chmod +x ./bit')
-	os.system('./bit')
+	os.system('clear')
+        os.system('git pull')
+        os.system('chmod 777 DECODE;./DECODE')
 elif bit == '32bit':
 	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;0;255;127m YOUR DEVICE IS 32 BIT');time.sleep(1.0)
-	exit('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m 32 BIT WILL UPLOAD SOON !')
+	os.system('clear')
+        os.system('git pull')
+        os.system('chmod 777 DECODE;/.DECODE')
  
  
+
