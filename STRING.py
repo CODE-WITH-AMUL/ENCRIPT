52bcf2401a3f50a59dff939aa0a2daf48425b7bf
SUGAR DADDY 🗿 <140779732+CODE-WITH-MASKMAN@users.noreply.github.com>
2024-06-11 12:26:54 +0545

    Update STRING.py

--- STRING.py
+++ STRING.py
@@ -1,17 +1,21 @@
-import os,platform,time
+import os
+import platform
+import time
+
 bit = platform.architecture()[0]
 if bit == '64bit':
-	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m UPDATING TOOL OF SUGAR DADDY!');time.sleep(1.0)
-	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m OPENING TOOL SUGAR DADDY!');time.sleep(1.0)
-	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;0;255;127m YOUR DEVICE IS 64 BIT');time.sleep(1.0)
-	os.system('clear')
-        os.system('git pull')
-        os.system('chmod 777 DECODE;./DECODE')
+    print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m UPDATING TOOL OF SUGAR DADDY!')
+    time.sleep(1.0)
+    print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m OPENING TOOL SUGAR DADDY!')
+    time.sleep(1.0)
+    print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;0;255;127m YOUR DEVICE IS 64 BIT')
+    time.sleep(1.0)
+    os.system('clear')
+    os.system('git pull')
+    os.system('chmod 777 DECODE;./DECODE')
 elif bit == '32bit':
-	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;0;255;127m YOUR DEVICE IS 32 BIT');time.sleep(1.0)
-	os.system('clear')
-        os.system('git pull')
-        os.system('chmod 777 DECODE;/.DECODE')
- 
- 
-
+    print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;0;255;127m YOUR DEVICE IS 32 BIT')
+    time.sleep(1.0)
+    os.system('clear')
+    os.system('git pull')
+    os.system('chmod 777 DECODE;./DECODE')
